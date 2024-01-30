b8ac578f624be5bb3879439328b2a636fc8b7623
Alejandro <alejandrocartagena68@gmail.com>
2024-01-30 12:54:57 -0500

    Added node event loop examples

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-const _ = require('lodash')
+const { readFile, writeFile } = require('fs')
 
-const items = [1, [2,[3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+console.log("start")
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}`,
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.log("Done with task")
+            }
+        )
+    })
+})
+console.log("starting next task")
